06958cce34746c5a783bcb7c6ee3b17f820b7e3d
decemberkaushik <kaushiknath014@gmail.com>
2021-01-14 19:05:03 +0530

    update main.js

--- res/main.js
+++ res/main.js
@@ -7,7 +7,7 @@ function boredButt() {
   var arr1 = [1, 2, 3, 4, 5]
   const partRan = arr1[Math.floor(Math.random() * arr1.length)];
   var intee = `http://www.boredapi.com/api/activity?participants=${partRan}`
-  async function arg() {
+  /*async function arg() {
     let info = await fetch(intee)
 
     const datai = await info.json()
@@ -28,10 +28,29 @@ function boredButt() {
     .catch(err =>{
       /*if (err.message=='') {
         alrM.innerHTML = ''
-      } else */{
+      } else {
         alrM.innerHTML = `<div class="alert alert-warning" role="alert">${err.message}
         </div>`
       }
+    })*/
+  
+  fetch(intee)
+    .then(response => {
+      return response.json();
     })
+    .then(datai => {
+      boredText.innerHTML = datai.activity
+      if (datai.link != "") {
+        forLink.innerHTML = `<br><a href="${datai.link}" class="card-link" target="_blank">${datai.link}</a>`
+        alrBox.innerHTML = ''
+      } else {
+        forLink.innerHTML = ''
+        alrBox.innerHTML = ''
+      }
+      })
+      .catch(err => {
+        alrM.innerHTML = `<div class="alert alert-warning" role="alert">${err.message}
+              </div>`
+        })
 }
 boredButton.addEventListener("click", boredButt);
